8a60910ec05dfb0844c75f042ac7d4908f62a4e9
Marcin Undefined <enclave.pevik@gmail.com>
2021-07-07 01:11:06 +0200

    feat(poke-details): display stats in alphabetical order

--- src/app/modules/poke-details/poke-details-page.component.ts
+++ src/app/modules/poke-details/poke-details-page.component.ts
@@ -9,6 +9,7 @@ import {
   OnInit,
   Output
 } from '@angular/core';
+import {IPokemonStat, IType} from "pokeapi-typescript";
 import {forkJoin, Subject} from "rxjs";
 import {switchMap, takeUntil, tap} from "rxjs/operators";
 import {DEFAULT_VISIBLE_POKE_DETAILS_COUNT} from "../../constants/defaults.constants";
@@ -62,10 +63,10 @@ export class PokeDetailsPageComponent implements OnInit, OnDestroy {
         .pipe(
           takeUntil(this.componentDestroyed$),
           tap(pokemon => {
-            this.pokeMoveNames = pokemon.moves.map(move => move.move.name);
+            this.pokeMoveNames = pokemon.moves.map(move => move.move.name).sort();
           }),
           tap(pokemon => {
-            this.pokeStats = pokemon.stats.map(stat => <PokemonStatistic>{
+            this.pokeStats = pokemon.stats.sort(this.pokeStatsSorter).map(stat => <PokemonStatistic>{
               statName: stat.stat.name,
               statValue: stat.base_stat,
             });
@@ -75,8 +76,8 @@ export class PokeDetailsPageComponent implements OnInit, OnDestroy {
               || pokemon.sprites.front_shiny_female;
           }),
           switchMap(pokemon => forkJoin(pokemon.types.map(type => this.api.getType(type.type.name)))),
-          tap(types => types.forEach(type => this.pokeNamesByType[type.name] = {
-            pokeNames: type.pokemon.map(poke => poke.pokemon.name),
+          tap(types => types.sort(this.pokeTypesSorter).forEach(type => this.pokeNamesByType[type.name] = {
+            pokeNames: type.pokemon.map(poke => poke.pokemon.name).sort(),
             visibleCount: this.initialVisibleCount,
           }))
         )
@@ -88,6 +89,26 @@ export class PokeDetailsPageComponent implements OnInit, OnDestroy {
           error => this.errorHandler.handleError(error)
         );
     }
+  }
+
+  private pokeStatsSorter(a: IPokemonStat, b: IPokemonStat): number {
+    if (a.stat.name < b.stat.name) {
+      return -1;
+    }
+    if (a.stat.name > b.stat.name) {
+      return 1;
+    }
+    return 0;
+  }
+
+  private pokeTypesSorter(a: IType, b: IType): number {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
   }
 
   public async changePoke(pokeName: string): Promise<void> {
